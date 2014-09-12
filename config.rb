d22a6ab17dca6fb45db982383488004766f30e8e
C. Sean Burns <sean.csb@gmail.com>
2014-09-12 17:10:30 -0400

    Setup and deployed.

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/dailylog"
+http_images_path = "/dailylog/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/dailylog/fonts"
+css_dir = "public/dailylog/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
